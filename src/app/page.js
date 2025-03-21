1da5ad1c30744aee77fd67c9a87ac24f6ed3f54a
Adrian Hassa <adrianhassa@gmail.com>
2025-03-21 17:56:06 +0100

    #1 Create initial home page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,102 +1,45 @@
-import Image from "next/image";
+import { Bot, User } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-full h-screen flex flex-col">
+      <header className="h-4"></header>
+      
+      <main className="text-center flex-grow flex">
+        <div className="w-1/5">
+          <p className="text-4xl text-primary pb-8">Vydra</p>
+          <div className="w-11/12 h-12 mb-6 mx-auto bg-primary rounded-2xl">
+            <p className="my-auto text-accent py-3">Previous bug report/chat...</p>
+          </div>
+          <div className="w-11/12 h-12 mb-6 mx-auto bg-primary rounded-2xl">
+            <p className="my-auto text-text py-3">Previous bug report/chat...</p>
+          </div>
+        </div>
+        <div className="w-4/5 bg-primary rounded-l-2xl py-4 flex flex-col justify-between">
+          <div className='w-full space-y-8'>
+            <div className='flex text-left text-xl mx-8'>
+              <Bot color='black' size={36} />
+              <p className='my-auto mx-4'>Hello! How can I help you today? ^^</p>
+            </div>
+            <div className='flex text-right text-xl mx-8 justify-end'>
+              <p className='my-auto mx-4'>Can you help me improve this bug report? I dont know to write good quality bug reports:(</p>
+              <User color='black' size={36} />
+            </div>
+            <div className='flex text-left text-xl mx-8'>
+              <Bot color='black' size={36} />
+              <p className='my-auto mx-4'>Certainly! I&apos;d be happy to help you improve your bug report. Please provide the details of the bug report you have, and I'll assist you in refining it.</p>
+            </div>
+          </div>
+          
+          <div className="p-4 bg-secondary text-primary w-11/12 mx-auto rounded-2xl flex">
+            <textarea className="w-full h-24 resize-none" placeholder="Type your message here..."></textarea>
+            <button className="bg-accent text-primary py-2 px-4 rounded h-12 my-auto mx-2">Send</button>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="p-2 text-center text-primary">
+        <p>Copyright © 2025 Adrian Hassa, Ionel Pop, Teodora Portase</p>
       </footer>
     </div>
   );
