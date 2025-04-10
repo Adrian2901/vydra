ab644d2f3f6a9a0d0328afa26192f512192c1cd6
Ionel Pop <ionelalexandru.pop@gmail.com>
2025-04-10 21:17:51 +0200

    Add Redis connection

---
+++ src/app/api/chatIds/route.js
@@ -0,0 +1,29 @@
+import redis from "@/lib/redis";
+
+export async function GET(req) {
+  try {
+    // Chat ids are stored in a sorted set where the weights are the timestamps
+    const chatIds = await redis.zrange("chatIds", 0, -1);
+    return new Response(JSON.stringify(chatIds), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
+
+export async function POST(req) {
+  try {
+    const { chatId } = await req.json();
+    console.log("Chat ID: ", chatId);
+    // Add the chat ID to the sorted set with the current id (timestamp) as the score
+    await redis.zadd("chatIds", chatId, chatId);
+    return new Response(JSON.stringify({ message: "Chat saved" }), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
