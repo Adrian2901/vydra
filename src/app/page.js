a2d010307f27e0e124ee99e0708170af5ca2801a
Ionel Pop <ionelalexandru.pop@gmail.com>
2025-04-10 21:14:12 +0200

    #11 Update page.js
    Add chat list sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,68 @@
-import Chat from '../app/components/chat';
+"use client";
+import React from "react";
+import Chat from "../app/components/chat";
+import ChatButton from "../app/components/chat-button";
+import { MessageSquarePlus } from "lucide-react";
 
 export default function Home() {
+  // Hook to fetch chat IDs from Redis
+  const [chatIds, setChatIds] = React.useState([]);
+  // Hook to manage the current chat ID
+  const [currentChatId, setCurrentChatId] = React.useState(null);
+
+  const handleNewChat = () => {
+    // Reset currentChatId to null when creating a new chat
+    setCurrentChatId(new Date().getTime());
+  };
+
+  // Fetch chat IDs from Redis
+  const getChatIds = async () => {
+    const response = await fetch("/api/chatIds");
+    const data = await response.json();
+
+    return data || []; // Return an empty array if no data is found
+  };
+
+  // Fetch chat IDs on component mount
+  React.useEffect(() => {
+    getChatIds().then((ids) => {
+      setChatIds(ids);
+      if (ids.length > 0) {
+        setCurrentChatId(ids[0]); // Set the first chat ID as the default
+      } else {
+        setCurrentChatId(null); // No chats available
+      }
+    });
+  }, []);
+
   return (
     <div className="w-full h-screen flex flex-col">
       <header className="h-4"></header>
-      
+
       <main className="text-center flex-grow flex">
-        <div className="w-1/5">
+        <div className="w-1/5" id="sidebar">
           <p className="text-4xl text-primary pb-8">Vydra</p>
-          <div className="w-11/12 h-12 mb-6 mx-auto bg-primary rounded-2xl">
-            <p className="my-auto text-accent py-3">Previous bug report/chat...</p>
+          <div>
+            <button
+              className="w-auto text-primary bg-accent rounded-2xl p-4 mb-4 hover:bg-primary hover:text-accent"
+              onClick={handleNewChat}
+            >
+              <MessageSquarePlus className="inline mr-2" />
+              New Chat
+            </button>
+            <div className="flex flex-col justify-center items-center space-y-2">
+              {chatIds.map((id) => (
+                <ChatButton
+                  key={id}
+                  chatId={id}
+                  onClick={() => setCurrentChatId(id)} // Update currentChatId on click
+                />
+              ))}
+            </div>
           </div>
         </div>
         <div className="w-4/5 bg-primary rounded-l-2xl py-4 flex flex-col justify-between">
-            <Chat />
+          <Chat chatId={currentChatId} />
         </div>
       </main>
 
